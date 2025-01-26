ad48a296bdb3000331a010d05b4dc866c3f54e78
Febi Salsabila <febisalsabillah05@gmail.com>
2025-01-26 16:24:34 +0700

    skema database1

--- app/Models/Kategori_Buku.php
+++ app/Models/Kategori_Buku.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Buku;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Kategori_Buku extends Model
 {
     use HasFactory;
+    {
+        return $this->hasMany(Buku::class);
+    }
 }
